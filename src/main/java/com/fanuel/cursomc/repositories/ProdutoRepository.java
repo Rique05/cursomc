444e0328eb6e54aa4072121b31032e2ad6c5fe1f
Fanuel Pereira <riquezerofive@gmail.com>
2018-12-02 16:29:57 -0200

    Produto e associacao muitos para muitos

---
+++ src/main/java/com/fanuel/cursomc/repositories/ProdutoRepository.java
@@ -0,0 +1,11 @@
+package com.fanuel.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.fanuel.cursomc.domain.Produto;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Integer>{
+
+}
