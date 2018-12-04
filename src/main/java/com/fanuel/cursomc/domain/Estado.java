179b55880615b8cab52262c1b3ac36f7db87e794
Fanuel Senac <riquezerofive@gmail.com>
2018-12-04 14:44:31 -0200

    Estado e cidade

--- src/main/java/com/fanuel/cursomc/domain/Estado.java
+++ src/main/java/com/fanuel/cursomc/domain/Estado.java
@@ -8,28 +8,21 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.ManyToMany;
-
-import com.fasterxml.jackson.annotation.JsonManagedReference;
+import javax.persistence.OneToMany;
 
 @Entity
-public class Categoria implements Serializable{
+public class Estado implements Serializable{
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
 	
-	@JsonManagedReference //Anotação que deve ser feita no lado que deve vir os objetos associados (Produtos das categorias) 
-	@ManyToMany(mappedBy="categorias") //Relacionamento mapeado no domínio Produto no atributo categorias
-	private List<Produto> produtos = new ArrayList<>();
-	
-	public Categoria(){
-		
-	}
+	@OneToMany(mappedBy="estado")
+	private List<Cidade> cidades = new ArrayList<>();
 
-	public Categoria(Integer id, String nome) {
+	public Estado(Integer id, String nome) {
 		super();
 		this.id = id;
 		this.nome = nome;
@@ -50,15 +43,14 @@ public class Categoria implements Serializable{
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
-	
-	public List<Produto> getProdutos() {
-		return produtos;
+
+	public List<Cidade> getCidades() {
+		return cidades;
 	}
 
-	public void setProdutos(List<Produto> produtos) {
-		this.produtos = produtos;
+	public void setCidades(List<Cidade> cidades) {
+		this.cidades = cidades;
 	}
-	
 
 	@Override
 	public int hashCode() {
@@ -76,7 +68,7 @@ public class Categoria implements Serializable{
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		Categoria other = (Categoria) obj;
+		Estado other = (Estado) obj;
 		if (id == null) {
 			if (other.id != null)
 				return false;
@@ -84,5 +76,6 @@ public class Categoria implements Serializable{
 			return false;
 		return true;
 	}
-
+	
+	
 }
